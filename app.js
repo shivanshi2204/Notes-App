e33737d93c82dcba06be1fe08f92741717e5a69e
shivanshi2204 <56682838+shivanshi2204@users.noreply.github.com>
2020-07-13 02:16:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,13 @@ yargs.command({
 yargs.command({
     command: 'read',
     describe:  'Reading a note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
     handler(){
         console.log('Reading a note!')
     }
