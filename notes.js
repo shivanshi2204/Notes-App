7fd41f0bb798e615866c83d7e8f57962451515e9
shivanshi2204 <56682838+shivanshi2204@users.noreply.github.com>
2020-07-12 18:59:04 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -4,18 +4,9 @@ const getNotes= ()=>{
     return 'Your notes...'
 }
  
-const addNote= (title,body) => {   //function responsible for adding a note
+const addNote= (title,body) => {   
     const notes= loadNotes()
-    //console.log(notes)
-    //check if the title already exists by 'array filter method'
-
-    //const duplicateNotes= notes.filter(function(note){
-      //  return note.title=== title   //if we returj true, filter is going to keep that individual note in the new array calling it a duplicate if we return false, it will no keep it into the array saying its not ab duplicate
-    //})
-
     const duplicateNotes= notes.filter((note) => note.title=== title)
-          //if we returj true, filter is going to keep that individual note in the new array calling it a duplicate if we return false, it will no keep it into the array saying its not ab duplicate
-    //in place of above, we can also stop the process once we find duplicates
     const duplicateNote = notes.find((note) => note.title===title)
 
     if(!duplicateNote){
@@ -32,7 +23,7 @@ const addNote= (title,body) => {   //function responsible for adding a note
 }
 
 const removeNote= (title) => {
-    //console.log(title)
+    
     const notes= loadNotes()
 
     const notesToKeep= notes.filter((note)=> note.title!== title)
@@ -60,8 +51,6 @@ const listNotes= () => {
 
 }
 
-
-
 const saveNotes= (notes)=> {   //stringify the data and save it in file system
     const dataJSON= JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
@@ -69,18 +58,16 @@ const saveNotes= (notes)=> {   //stringify the data and save it in file system
 
 const loadNotes= () => {
     try{
-        const dataBuffer= fs.readFileSync('notes.json')  //1. either make a file 2. write a defensive code that runs even if the file doesn't exist
+        const dataBuffer= fs.readFileSync('notes.json')  
         const dataJSON= dataBuffer.toString()
         return JSON.parse(dataJSON)
     } catch(e){
         return []   //first item will be sved into this array
-    }
-
-    
+    }   
      
 }
 
-module.exports= {   //to export many items, instead of setting it equal to a fn, we will be setting it equal to an object instead
+module.exports= {   
     getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
